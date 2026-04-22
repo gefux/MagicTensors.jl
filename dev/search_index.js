e64a8b580605fdf93aed5712fc1459a07e47a4c4
Documenter.jl <documenter@juliadocs.github.io>
2026-04-22 14:41:52 +0000

    build based on 71e82f5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#MagicTensors","page":"Home","text":"Documentation for MagicTensors.\n\n","title":"MagicTensors"},{"category":"constant","location":"#MagicTensors.MAGIC","page":"Home","text":"The magic number. \n\n\n\n\n\n","title":"MagicTensors.MAGIC"}]
+}
